784da98c1737e22b1172a18ced56f91cf70238f5
Bryan Donovan <bdondo@gmail.com>
2013-05-20 09:10:24 -0700

    malt shop ruby solution

---
+++ 02_malt_shop/malt.rb
@@ -0,0 +1,27 @@
+require 'pp'
+
+runs = 1000000
+lil_dur = 5
+bill_dur = 5
+hits = 0
+
+runs.times do
+  lil_start = Random.rand(30.0)
+  bill_start = Random.rand(30.0)
+
+  lil_end = lil_start + lil_dur
+  bill_end = bill_start + bill_dur
+
+  if lil_start > bill_start && lil_start < bill_end
+    hits += 1
+  elsif bill_start > lil_start && bill_start < lil_end
+    hits += 1
+  elsif bill_start < lil_start && bill_end > lil_end
+    hits += 1
+  elsif lil_start < bill_start && lil_end > bill_end
+    hits += 1
+  end
+end
+
+puts
+puts hits.to_f/runs
